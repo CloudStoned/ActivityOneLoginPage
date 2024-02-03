8761f6d1694a706b82e17b5444e72b813a8c964c
CloudStone <92814911+CloudStoned@users.noreply.github.com>
2024-02-03 11:26:56 +0800

    LoginForm Complete

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import githublogo from './components/assets/githublogo.png'
 import './App.css';
 import LoginForm from './components/LoginForm/LoginForm';
 import SubForm  from './components/LoginForm/SubForm';
+import Footer  from './components/LoginForm/Footer';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
       <div className='github-logo'>
         <img src={githublogo} />
       </div>
       <div className='sign-in'>Sign in to GitHub</div>
       <br></br>
+      <div className='forms-container'>
         <LoginForm />
         <SubForm />
+      </div>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/LoginForm/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return(
+        <footer>
+            <a href="#">Terms</a>
+            
+            <a href="#">Privacy</a>
+
+            <a href="#">Contact GitHub Support</a>
+
+            <a href="#">Manage cookies</a>
+
+            <a href="#">Do not share my personal information</a>
+        </footer>
+    );
+};
+
+export default Footer;
+
