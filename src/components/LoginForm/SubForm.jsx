6697f8f6c95444ee0f8de7a97f3045d9b7a3a6ff
CloudStone <92814911+CloudStoned@users.noreply.github.com>
2024-02-03 10:18:00 +0800

    Added: Complete Subform

--- src/components/LoginForm/SubForm.jsx
+++ src/components/LoginForm/SubForm.jsx
@@ -5,7 +5,15 @@ import './SubForm.css';
 const SubForm = () => {
     return(
         <div className="subform-wrapper">
-            <div className="sign-in-with-a-passkey"> Sign in with a passkey </div>
+            <div className="sign-in-with-a-passkey"> 
+             <a href="">Sign in with a passkey</a> 
+            </div>
+            <div className="sign-in-new-to">
+                <div className="new-to-github">New to GitHub? </div>
+                <div className="create-an-account">
+                    <a href="#">Create an account</a>
+                </div>
+            </div>
         </div>
     );
 };
